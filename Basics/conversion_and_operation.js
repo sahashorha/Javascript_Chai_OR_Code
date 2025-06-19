d308013a843d97d749a7afe034aae6b6e96fb3a8
Rahul Poddar <102842190+sahashorha@users.noreply.github.com>
2025-06-19 19:43:40 +0000

    Operation done -> mathematical

--- Basics/conversion_and_operation.js
+++ Basics/conversion_and_operation.js
@@ -20,7 +20,42 @@ let booleanIsLoggedIn =Boolean(isLoggedIn);
 
 let sumNumber = 33;
 let stringNumber = String(sumNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // string conversion done -> (string)
+
+
+
+
+// ********************** Operations ******************************
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2*2);
+// console.log(3*3);
+
+let str1 = " Hello ";
+let str3 = " Hitesh";
+console.log(str1 + str3);
+
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(3 % 4 + 5 );
+console.log(true);
+
+// +true -> is right -> 1 
+
+
+let num1,num2, num3
+num1 = num2 = num3 = 2 + 2;
+console.log(num1);
+
+let gamecounter = 100;
+gamecounter++;
+// prefix -> operator increment use before and 
+// postfix -> operator increment after use only
+console.log(gamecounter);
