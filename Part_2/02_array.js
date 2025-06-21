1aab514b79b0e458cf55e40e6b53eaf3733cd29f
Rahul Poddar <102842190+sahashorha@users.noreply.github.com>
2025-06-21 18:00:12 +0000

    array

---
+++ Part_2/02_array.js
@@ -0,0 +1,13 @@
+ const marvel_heroes = ["thor", "Ironman","spiderman"]
+ const dc = ["superman","flash","batman"]
+//  marvel_heroes.push(dc);
+//  console.log(marvel_heroes);
+ 
+//  const allHeroes = marvel_heroes.concat(dc);
+// console.log(allHeroes);
+
+// const all_new_heroes = [...marvel_heroes,...dc];
+// console.log(all_new_heroes);
+
+const another_array = [1,2,3,[4,5,6],3,[3,4,5,6]]
+const real_another_array = another_array.flat(Infinity);
