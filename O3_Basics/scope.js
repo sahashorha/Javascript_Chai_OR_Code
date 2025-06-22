a03de7a5d67ff66253e36e5cdc5c6da5c99a2594
Rahul Poddar <102842190+sahashorha@users.noreply.github.com>
2025-06-22 16:33:12 +0000

    All about scope level

---
+++ O3_Basics/scope.js
@@ -0,0 +1,40 @@
+let a= 3000;
+if(true){
+let a = 10;
+const b = 20;
+var c = 30;
+console.log(a);
+   
+}
+
+// console.log(a);
+console.log(c);
+// console.log()
+
+// Hosting 
+// two types of function declaration 
+
+function addOne(num){
+    return num + 1;
+}
+addOne();
+const addTwo = function (num){
+    return num + 2;
+}
+
+console.log(addTwo(5));
+
+if(true){
+    const username = "Rahul"
+    if(username === "Rahul"){
+        const website = " Poddar"
+        console.log(username + website);
+    }
+    // console.log(website)
+    console.log("Hello Rahul Poddar");
+}
+
+
+// +++++++++++++++++++++++++++ Interesting  +++++++++++++++++++++++++++
+// This video all is all about the details of scope -> scope variable 
+// function 
