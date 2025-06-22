5ba3089d33a17dfc0713d9a0ef5e42a1c4b73a4f
Rahul Poddar <102842190+sahashorha@users.noreply.github.com>
2025-06-22 12:23:11 +0000

    Destructuring in objects

--- Part_2/04_Objects.js
+++ Part_2/04_Objects.js
@@ -1,70 +1,89 @@
-const tinderUser = new Object(); //-> singleton objects 
+// const tinderUser = new Object(); //-> singleton objects 
 
-console.log(tinderUser);
-tinderUser.id ="34sdf";
-tinderUser.name = "rahult" 
-tinderUser.isLoggedIn = false;
-console.log(tinderUser);;
+// console.log(tinderUser);
+// tinderUser.id ="34sdf";
+// tinderUser.name = "rahult" 
+// tinderUser.isLoggedIn = false;
+// console.log(tinderUser);;
 
-// creating objects , nested objects 
-const regularUser= {
-    email : "sonemone@gmail.com",
-    fullname :{
-        userFullName :{
-            firstname : "rahul",
-            lastname : "Poddar"
-        }
-    }
-}
-// the method how we are finding the values in nested functions 
-// console.log(regularUser.fullname.userFullName.firstname);
-// objects inside object is accessed thorough . nesting 
+// // creating objects , nested objects 
+// const regularUser= {
+//     email : "sonemone@gmail.com",
+//     fullname :{
+//         userFullName :{
+//             firstname : "rahul",
+//             lastname : "Poddar"
+//         }
+//     }
+// }
+// // the method how we are finding the values in nested functions 
+// // console.log(regularUser.fullname.userFullName.firstname);
+// // objects inside object is accessed thorough . nesting 
 
-const obj1 = {
-    1:"a",
-    2 :"b"
-}
-const obj2 = {
-    3:"a",
-    4 :"b"
-}
+// const obj1 = {
+//     1:"a",
+//     2 :"b"
+// }
+// const obj2 = {
+//     3:"a",
+//     4 :"b"
+// }
 
-// how we are joined to the const -> by using the assign objects
-// const obj3 = {obj1 , obj2}
-// console.log(obj3);
-const obj3 = Object.assign({},obj1, obj2);
+// // how we are joined to the const -> by using the assign objects
+// // const obj3 = {obj1 , obj2}
+// // console.log(obj3);
+// const obj3 = Object.assign({},obj1, obj2);
 
-console.log(obj3);
-// This is another method is also used obj1 , obj2 bny the dot method             
+// // console.log(obj3);
+// // This is another method is also used obj1 , obj2 bny the dot method             
 
-const obj4 = {...obj1, ...obj2};
-console.log(obj3);
+// // const obj4 = {...obj1, ...obj2};
+// // console.log(obj3);/
 
-const users = [
-    {
-        id:1,
-        email:"rahui@gmail.com",
-    },
-    {
+// const users = [
+//     {
+//         id:1,
+//         email:"rahui@gmail.com",
+//     },
+//     {
 
-    },
-    {
+//     },
+//     {
 
-    }
-]
-users[1].email
-console.log(tinderUser);
+//     }
+// ]
+// // users[1].email
+// console.log(tinde/rUser);
 // from database this date come in this format -> 
 // object of arrays 
 // finding in the keys of the object 
-console.log(Object.keys(tinderUser));
+// console.log(Object.keys(tinderUser));
 // finding the values of the keys
-console.log(Object.values(tinderUser));
+// console.log(Object.values(tinderUser));
 
 // finding the entries of the objects 
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 // finding the what property is used 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+const course = {
+    courseName : "Atomic Habits ",
+    price : "987",
+    courseInstructor : "rhaul"
+} 
+// Dstructering methods -> courseInstructor => Instructor
+const {courseInstructor:instructor} = course;
 
+console.log(instructor);
 
+const navbar = ({company}) ={
+
+}
+
+// {
+//     name:"Rahul",
+//     courseName : "js in hindio",
+//     price : "free"
+// }
+
+ 
