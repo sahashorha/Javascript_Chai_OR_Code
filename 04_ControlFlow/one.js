cb54640827a0f2e1a94a29bd22e566378af713d8
Rahul Poddar <102842190+sahashorha@users.noreply.github.com>
2025-06-23 15:38:57 +0000

    One.js

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,35 @@
+// if
+// const isUserLoggedIn = true; 
+// if(true){
+ 
+// }
+// const score = 200;
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`user power  ${power}`);
+// }
+
+const balance = 1000;
+// if(balance > 500) console.log("test");
+ 
+if(balance < 500){
+    console.log("less than 500");
+}
+else if(balance < 750){
+    console.log("less than 750")
+}
+else if(balance < 900){
+    console.log("Less 900")
+}
+
+const userLoggedIn = true
+const debitCard = true;
+if(userLoggedIn && debitCard && 2 == 2){
+    console.log("Allow to buy course");
+}
+const LoggedInfromGoogle = false;
+const LogggedInfromEmail = true;
+if(LoggedInfromGoogle || LogggedInfromEmail){
+    console.log("user logged it");
+}
+
