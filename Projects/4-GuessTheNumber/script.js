33b1f856d4d85816c91abd9f3c363b24d3d01ff4
Rahul Poddar <poddarrahul58@gmail.com>
2025-06-30 00:38:21 +0530

    guess the number

--- Projects/4-GuessTheNumber/script.js
+++ Projects/4-GuessTheNumber/script.js
@@ -1,4 +1,4 @@
-const randomNumber = parseInt(Math.random() * 100 + 1);
+let                                         randomNumber = parseInt(Math.random() * 100 + 1);
 const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField')
 const guessSlot = document.querySelector('.guesses');
@@ -68,14 +68,31 @@ function displayMessage(message){
 
 function displayguess(guess){
    userInput.value = '';
-      guessSlot.innerHTML += `${guess  }`
+      guessSlot.innerHTML += `${guess},   `
       numGuess++;
       lastresult.innerHTML = `${11 -numGuess}`;
 }
 
 function newGame(){
-
+      const newGameButton = document.querySelector('#newGame')
+      newGameButton.addEventListener('click', function(){
+ randomNumber = parseInt(Math.random() * 100 + 1);
+ prevGuess = [];
+ numGuess = 1;
+ guessSlot.innerHTML = '';
+      lastresult.innerHTML = `${11 -numGuess}`;
+      userInput.removeAttribute('disabled','');
+      startOver.removeChild(p)
+      playGame = true;
+      })
 }
 function endGame(){
-
+userInput.value = ''
+userInput.setAttribute('disabled','')
+p.classList.add('button')
+p.innerHTML = `<h2 id="newGame">Start New Game </h2>`
+// startOver.appendChild()
+startOver.appendChild(p);
+playGame = false;
+newGame();
 }
